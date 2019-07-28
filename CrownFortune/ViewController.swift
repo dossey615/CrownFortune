518c106fc1d25cd5586be3f36aca1559108cfdaa
dossey615 <dossey615@gmail.com>
2019-07-28 22:14:04 +0900

    Initial Commit

---
+++ CrownFortune/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrownFortune
+//
+//  Created by 土居将史 on 2019/07/28.
+//  Copyright © 2019 土居将史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
